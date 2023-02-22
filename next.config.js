32d7eaf72cad4eba39ca41a05f48fbf7880e0e4b
Anele Mbabela <anelembabela@gmail.com>
2023-02-23 00:21:56 +0200

    1st link commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.themealdb.com",
+        port: "",
+        pathname: "/images/media/meals/**",
+      },
+    ],
+  },
+};
+
+//localhost:3000/(https:/www.themealdb.com/images/media/meals/uuqvwu1504629254.jpg)
+
+http: module.exports = withBundleAnalyzer(nextConfig);
